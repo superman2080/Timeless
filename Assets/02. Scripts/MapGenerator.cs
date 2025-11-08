f1077c60f6ee1d68debe643048a1b21608e3b245
superman2080 <superman2080@naver.com>
2025-11-08 14:14:58 +0900

    add: folders

---
+++ Assets/02. Scripts/MapGenerator.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private GameObject tile;
+    public Transform Pool => transform;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
